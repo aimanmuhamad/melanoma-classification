854e34137f6f32094d022c5890f49029760fcf23
aimanm135 <aimanm135@gmail.com>
2022-09-28 15:02:37 +0700

    version 1

---
+++ preprocessing_image.py
@@ -0,0 +1,16 @@
+from preprocessing_module import *
+
+global_folder = YOUR_FOLDER
+
+folderpaths = [global_folder + 'NotMelanoma',
+              global_folder + 'Melanoma'
+              ]
+'''
+input params:
+folderpaths : List of the folderpaths for specific Plant
+labels : List of labels 
+savepath : Path to export datasheet
+'''
+labels = [0,1]
+savepath = YOUR_FOLDER
+process_plant(folderpaths, labels, savepath)
